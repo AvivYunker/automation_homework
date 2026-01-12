2b65dc907eaabd2facd46d0db14146c6c0ce9efd
Aviv Yunker <avivyunker@gmail.com>
2026-01-12 11:11:28 +0200

    work on initial version of init file

---
+++ pages/__init__.py
@@ -0,0 +1,23 @@
+'''
+The "__init__.py" file tells Python that
+the folder is a package (a collection of
+Python modules).
+
+We need this file because without it - Python
+won't recognize the folder as a package.
+
+'''
+
+from base_page import BasePage
+from .login_page import LoginPage
+from .search_page import SearchPage
+from product_page import ProductPage
+from .cart_page import CartPage
+
+__all__ = [
+    'BasePage',
+    'LoginPage',
+    'SearchPage',
+    'ProductPage',
+    'CartPage'
+]
